9753aa7386df69dbbf790814551445a5c4c017ff
WillJaya <williamanandaw@gmail.com>
2026-06-24 13:44:01 +0700

    Revert "Su san to"
    This reverts commit 2c8a4a07ec7ed2ce739ecfd190afae2b5f9d6882.

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -50,11 +50,6 @@ class PurchaseOrder(models.Model):
     pay_to = fields.Many2one('res.bank')
 
     # WADIDAAAAWWWWWWW
-    # su
-    # san
-    # to
-
-
 
     #Freight
     purchase_freights = fields.One2many(comodel_name="purchase_order_freight", inverse_name="purchase_order_id")
